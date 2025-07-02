8c1beba8b13ed0e23c3667349db7e056b3fe693c
peyha <peyha.nicole@outlook.fr>
2025-07-02 17:26:36 +0200

    api

--- apis/src/db/schema/Listener.ts
+++ apis/src/db/schema/Listener.ts
@@ -3,11 +3,8 @@ import { pgEnum, pgTable as table } from "drizzle-orm/pg-core";
 import * as t from "drizzle-orm/pg-core";
 import { db, types } from "@duneanalytics/sim-idx";
 
-export const poolCreated = table("pool_created", {
-  chainId: db.uint64('chain_id'),
-  caller: db.address('caller'),
-  pool: db.address('pool'),
-  token0: db.address('token0'),
-  token1: db.address('token1'),
-  fee: db.uint24('fee'),
+export const preLiquidationHealth = table("pre_liquidation_health", {
+  chainbytes32: db.uint64('chainbytes32'),
+  txHash: db.bytes32('tx_hash'),
+  ltv: db.uint256('ltv'),
 })
